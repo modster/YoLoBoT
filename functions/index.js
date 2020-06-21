160bdf0ed9c260a8c0281f61779a44bbfd486f92
Mike Greeff <unmodster@gmail.com>
2020-06-21 08:50:37 -0600

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -13,24 +13,7 @@ const exchangeId = "binance",
     timeout: 30000,
     enableRateLimit: true,
   });
-
-// S a n d b o x  M o d e :
-exchange.setSandboxMode(true);
-
-(async () => {
-  try {
-
-    // F e t c h  B T C  B a l a n c e
-    let binanceBalance = await exchange.fetchBalance();
-    console.log(binanceBalance.BTC.free);
-    
-    // I m p l i c i t l y  F e t c h  A l l  B a l a n c e s
-    // console.log(await exchange.fetchBalance());
-
-  } catch (error) {
-    console.log(error);
-  }
-})();
+  
 exports.btc = functions.https.onRequest((req, res) => {
   const hours = (new Date().getHours() % 12) + 1; // London is UTC + 1hr;
   const x = binanceBalance.BTC.free;
